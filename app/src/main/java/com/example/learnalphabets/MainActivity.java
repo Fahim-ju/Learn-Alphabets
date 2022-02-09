66d3a700f66d26676a3a56f911a6d79894cc36e3
Fahim-ju <f.nahian1997@gmail.com>
2022-02-09 22:40:09 +0600

    New activity for WordView

--- app/src/main/java/com/example/learnalphabets/MainActivity.java
+++ app/src/main/java/com/example/learnalphabets/MainActivity.java
@@ -2,10 +2,18 @@ package com.example.learnalphabets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String letter_key="letter";
     private ListView listView;
     private String[] lettersName;
     private int[] lettersImage = {R.drawable.thisis,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,
@@ -23,5 +31,39 @@ public class MainActivity extends AppCompatActivity {
         customAdapter = new CustomAdapter(MainActivity.this,lettersName,lettersImage);
         System.out.println("Yahooooo");
         listView.setAdapter(customAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this,LetterDetails.class);
+
+                intent.putExtra(letter_key,String.valueOf(i));
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        dialogBuilder.setIcon(R.drawable.ic_baseline_exit_to_app_24);
+        dialogBuilder.setTitle("Alert");
+        dialogBuilder.setMessage("Do you sure Want to exit?");
+        dialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        dialogBuilder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        dialogBuilder.setCancelable(false);
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+        //super.onBackPressed();
     }
 }
